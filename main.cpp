b91822e75501f58b7720c608b9fbe1df75bab10e
rawiphonyai <rawiphon.yai@student.mahidol.edu>
2026-03-23 09:55:40 +0000

    Week 11 student

--- main.cpp
+++ main.cpp
@@ -2,41 +2,64 @@
 using namespace std;
 
 #include "student.h"
-#define N 3
 int main(int argc, char* argv[]) {
-  student s1,s2;
-
- s1.set_name("Amy"); s1.set_age(20); s2.set_name("James"); s2.set_age(17); s1.get_age()>s2.get_age() ? s1.print_name(); cout<< "is older":s2.print_name(); cout <<"is older";
-
-
-
+  int N = (argc-1)/2;
   student a[N];
-  // Set age and set name for the first two ppl
-  int i,age;
+
+
+  int i,j,age,b;
   string n;
+
    // 1. Ask for N names and N age using cin
- 
+    for(i=1,j=0;i<argc;i+=2,j++){
+      a[j].set_name(argv[i]);
+      a[j].set_age(atoi(argv[i+1]));
+  }
+
+
+   /*
+  for(i=1;i<N;i++){
+    cout<<"Student No."<<i<<endl;
+    cout<<"Name : "<<endl;
+    cin>>n;
+    a[i].set_name(n);
+    cout<<"Age : "<<endl;
+    cin>>b;
+    a[i].set_age(b);
+  }
+  */
   
  
   //2. Print name and age of all N ppl
-  
+  for(i=0;i<N;i++){
+    a[i].display();
+  }
   
   
   //3. Print name and age of 
   //the youngest person
   //Find what the age of the youngest person
- 
+  student youngest = a[0];
+
+  for(i=1;i<N;i++){
+    if(youngest.get_age()>a[i].get_age()) youngest = a[i];
+  }
+
   
   // Print all info for the yougest person
- 
+    cout<<"Youngest student"<<endl;
+    
+    for(i=0;i<N;i++){
+    if(youngest.get_age() == a[i].get_age()){
+        a[i].display();
+    }
+  }
   
   
   //4. Change input from cin to argv
 
-  
- return 0;
 
-    
+ return 0;  
   }
   
   
--- student.h
+++ student.h
@@ -1,14 +1,41 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
 using namespace std;
 
 class student{
 private:
-
   string name;
+  int age;
 
 public:
-  
-   void set_name(string="Yme");
-    void print_name();
-
+  void set_name(string);
+  void print_name();
+  void set_age(int);
+  int get_age();
+  void display();
 };
+
+void student::set_name(string n){
+  name=n; 
+}
+
+void student::print_name(){
+  cout<<"Name: "<<name<<endl;
+}
+
+void student::set_age(int b){
+  age=b;
+}
+
+int student::get_age(){
+  return age;
+}
+
+void student::display(){
+  print_name();
+  cout<<"Age:"<<" "<<age<<endl;
+}
+
+// 1. set_age --> set ค่า age 
+// 2. get_age --> return ค่า age
